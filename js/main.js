0f52e96a724966d31b02152b170556bcec8e0e94
daniel331 <daniel331@gmail.com>
2015-08-15 23:22:48 +0300

    Fixed tabs behavior and implemented 2nd tab

--- js/main.js
+++ js/main.js
@@ -1,29 +1,42 @@
 
-
-
 function setHash() {
-	/*window.location.hash = 'hello';
-	return false;*/
-	if(history.pushState)
-	{
-    	history.pushState(null, null, '#myhash');
-	}
+	var pageTop = $(window).scrollTop();
+		document.location.hash = $(this).attr("href");
+		$(window).scrollTop(pageTop);
+		return false;	
 }
 
+
 function setTab() {
+	var tabName = document.location.hash;
+	tabName = tabName.substring(1);
+	var tabBtn = tabName + "-tabBtn";
+	unselectAllTabs();
+
+	// Select current tab
+	$('#' + tabBtn).addClass('active');
+
+	$(".tab-content").attr('hidden', 'hidden');
+	$('#' + tabName).removeAttr('hidden');
 		
+}
+
+function unselectAllTabs() {
+	$(".tabs li").removeClass('active');
+}
+
+function openInNewTab()
+{
+	var win = window.open('http://www.paulirish.com', '_blank');
+  	win.focus();
 }
 
 
 $(document).ready(function(){
 	UTILS.addEvent(window, "hashchange", setTab);
+	$('#quickTabAnchor').click(setHash);
+	$('#fmyTabAnchor').click(setHash);
+	$('#teamTabAnchor').click(setHash);
+	$('#publicTabAnchor').click(setHash);	
 
-	//var innerAnchor = $('#quickTabBtn').children()[0];
-	//UTILS.addEvent(innerAnchor, "click", setTab);
-	$('#quickTabAnchor').click(function(event) {
-		var pageTop = $(window).scrollTop();
-		document.location.hash = $(this).attr("href");
-		$(window).scrollTop(pageTop);
-		return false;
-	});
 });
